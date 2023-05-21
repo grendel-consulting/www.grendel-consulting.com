52b07760266916435e9c46b639138cbdbaede884
James Ramirez <james.ramirez@grendel-consulting.com>
2023-05-21 07:03:31 +0100

    feat: recreate existing website

---
+++ README.md
@@ -0,0 +1,3 @@
+# Brochureware site for Grendel Consulting
+
+Deploys our corporate website to www.grendel-consulting.com, written in Gatsby and using CDKTF.
---
+++ frontend/website/src/pages/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import * as styles from "../styles/site.css";
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main className={styles.page}>
+      <h1 className={styles.heading}>Grendel Consulting</h1>
+      <p className={styles.paragraph}>
+        We are a boutique consultancy providing interim, advisory services in{" "}
+        the areas of innovation, product and technology management and{" "}
+        development, digital platforms, and cyber security.
+      </p>
+      <p className={styles.paragraph}>
+        We work with experienced founders and managers as well as those just{" "}
+        starting out in leadership roles, bringing a range of experience in top{" "}
+        tier enterprises and early stage statups.
+      </p>
+      <p className={styles.paragraph}>
+        We operate as a force multiplier, whether that be through: providing a{" "}
+        capability where you have a gap; delivery of coaching, mentoring or{" "}
+        workshops to enhance your existing team; or supporting them to deliver a{" "}
+        step-change to your business.
+      </p>
+      <footer className={styles.footer}>
+        <p>
+          Limited company, registered in England and Wales, with company number{" "}
+          10125435. Registered office: 25 Victoria Gardens, Burgess Hill, RH15{" "}
+          9NB
+        </p>
+        <p>
+          &copy; 2016-{new Date().getFullYear()} Grendel Consulting Limited. All{" "}
+          rights reserved.
+        </p>
+      </footer>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
