9edb79ba394c6a201dc3233298caaca93a09f99d
James Ramirez <james.ramirez@grendel-consulting.com>
2024-11-05 21:38:58 +0000

    test: proof-of-concept on accessibility testing
    Signed-off-by: James Ramirez <james.ramirez@grendel-consulting.com>

--- frontend/website/features/steps.ts
+++ frontend/website/features/steps.ts
@@ -1,6 +1,7 @@
 import { expect } from '@playwright/test';
 import { createBdd } from 'playwright-bdd';
 import { checkHttpUrl } from './utils';
+import AxeBuilder from '@axe-core/playwright'; 
 
 const { Given, When, Then } = createBdd();
 
@@ -27,4 +28,10 @@ When('I click link {string}', async ({ page }, name) => {
 Then('I see in title {string}', async ({ page }, keyword) => {
   const escapedKeyword = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   await expect(page).toHaveTitle(new RegExp(escapedKeyword), { timeout: 5000 });
+});
+
+Then('I have no problems navigating the site', async ({ page }) => {
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
 });
