cb33c8385f231dcaa645db33d2c1d708bcdb0f1f
James Ramirez <james.ramirez@grendel-consulting.com>
2025-08-05 19:25:45 +0100

    feat: redirect directory-esque paths to true S3 object
    Signed-off-by: James Ramirez <james.ramirez@grendel-consulting.com>

---
+++ infrastructure/static-site/stacks/spa-middleware.js
@@ -0,0 +1,20 @@
+// Based on https://aws.amazon.com/blogs/compute/implementing-default-directory-indexes-in-amazon-s3-backed-amazon-cloudfront-origins-using-lambdaedge/
+"use strict";
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+
+  let target = request.uri;
+
+  // Check whether the URI is a directory.
+  if (uri.endsWith("/")) {
+    target += "index.html";
+  }
+  // Check whether the URI is missing a file extension.
+  else if (!uri.includes(".")) {
+    target = request.uri + "/index.html";
+  }
+
+  request.uri = target;
+
+  return callback(null, request);
+};
